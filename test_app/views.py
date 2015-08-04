8c8bdf4742daaadd75e9d59751c04177b3b0a342
Daniel Greenfeld <danny@eventbrite.com>
2015-08-03 21:54:18 -0700

    Got basic view test working

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
@@ -7,6 +8,10 @@ from .models import Data
 
 
 class DataDetailView(SpammableMixin, View):
+
     @property
     def object(self):
         return get_object_or_404(Data, pk=self.kwargs['pk'])
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,20 +1,28 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseNotAllowed
+from django.test import RequestFactory
 
 from spam.views import ReportSpamCreateView
 from test_plus.test import TestCase
 
 from test_app.models import Data
+from test_app.views import DataDetailView
 
 
 class TestReportSpamCreateView(TestCase):
 
     def setUp(self):
         self.data = Data.objects.create(title='test')
+        self.factory = RequestFactory()
 
-    def test_display_report_view(self):
+    def test_data_detail_view(self):
+        """ Just to make certain we have the tests wired up right"""
+        # Get the URL
         url = reverse('data', kwargs={'pk': self.data.pk})
-        response = self.get(url)
+        # Create a request
+        request = self.factory.get(url)
+        # Generate a response
+        response = DataDetailView.as_view()(request)
 
-        # TODO: Make this actually test for useful things
-        self.assertTrue(isinstance(response, HttpResponseNotAllowed))
+        # Check to see if we had a 404 or not
+        self.response_200(response)
